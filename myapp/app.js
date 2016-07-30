ed3977c9201c68ba96f468d6cc427fd98b65ebbe
JoanGarciaCedano <Joan.GC.mail@gmail.com>
2016-07-30 01:51:44 -0500

    push

--- myapp/app.js
+++ myapp/app.js
@@ -456,6 +456,15 @@ var sensor = {
           hum = parseFloat(b.humidity.toFixed(2));
           socket.emit('temperatura', temp, date);
           socket.emit('humedad', hum, date);
+
+					//update a base de datos
+					MongoClient.connect(url, function(err, db) {
+					  assert.equal(null, err);
+
+					  actualizarBASE(db, temp, hum, function() {
+					      db.close();
+					  });
+					});//update bd
       }
   }
 };
@@ -480,14 +489,6 @@ sensor.read();
 	console.log("error GAS 2");
   }
 
-//update a base de datos
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-
-  actualizarBASE(db, temp, hum, function() {
-      db.close();
-  });
-});//update bd
 
 //mostrar BD
 MongoClient.connect(url, function(err, db){
