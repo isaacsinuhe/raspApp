8789dbc48a0e02fb68f714dc2dd9b19440f639b1
JoanGarciaCedano <Joan.GC.mail@gmail.com>
2016-08-02 19:12:59 -0500

    pruebas WS

--- myapp/app.js
+++ myapp/app.js
@@ -92,22 +92,24 @@ var Client = require('node-rest-client').Client;
 
 var client = new Client();
 
-// direct way
-//client.get("https://jsonplaceholder.typicode.com/posts/1", function (data, response) {
-    // parsed response body as js object
-  //  console.log(data);
-    // raw response
-  //  console.log(response);
-//});
-
 // registering remote methods
 client.registerMethod("jsonMethod", "https://jsonplaceholder.typicode.com/posts/1", "GET");
 
 client.methods.jsonMethod(function (data, response) {
     // parsed response body as js object
     console.log(data);
-    // raw response
-    //console.log(response);
+});
+
+var args = {
+    data: { title: "hello" },
+    headers: { "Content-Type": "application/json" }
+};
+
+client.registerMethod("putMethod", "https://jsonplaceholder.typicode.com/posts/1", "PUT");
+
+client.methods.putMethod(args, function (data, response) {
+    // parsed response body as js object
+    console.log(data);
 });
 
 
